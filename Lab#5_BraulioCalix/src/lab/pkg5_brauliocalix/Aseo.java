6a51697c60f8a5aac8ee09204c1716d9f22e9c18
BraulioCalix55 <djbrzhn1@hotmail.com>
2018-08-24 14:40:32 -0500

    clases y parte gui 2

--- Lab#5_BraulioCalix/src/lab/pkg5_brauliocalix/Aseo.java
+++ Lab#5_BraulioCalix/src/lab/pkg5_brauliocalix/Aseo.java
@@ -11,4 +11,47 @@ package lab.pkg5_brauliocalix;
  */
 public class Aseo {
     
+    private String nombre;
+    private String descrip;
+    private String funcion;
+
+    public Aseo() {
+    }
+
+    public Aseo(String nombre, String descrip, String funcion) {
+        this.nombre = nombre;
+        this.descrip = descrip;
+        this.funcion = funcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public void setDescrip(String descrip) {
+        this.descrip = descrip;
+    }
+
+    public String getFuncion() {
+        return funcion;
+    }
+
+    public void setFuncion(String funcion) {
+        this.funcion = funcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Aseo{" + "nombre=" + nombre + ", descrip=" + descrip + ", funcion=" + funcion + '}';
+    }
+
+
 }
